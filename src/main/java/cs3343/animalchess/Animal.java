b35466348268fc5fcccfabd57bc51cfb56653997
Kjr00 <89450959+Kjr00@users.noreply.github.com>
2023-10-03 23:06:07 +0800

    Renew assets for better visual clarity, Wrote pseudo-code for Board.isInWater() and isOutBound(), Changed comments in Animal

--- src/main/java/cs3343/animalchess/Animal.java
+++ src/main/java/cs3343/animalchess/Animal.java
@@ -21,13 +21,14 @@ public abstract class Animal {
 
         board.addAnimal2Board(this, x, y);
     }
-    //rules for general:
-    //1. Cannot move more than one block
-    //2. X and Y cannot be the same as original location
-    //3. Cannot move outside of chess board
-    //4. Cannot enter river
-    //5. Cannot move into friendly animal or trap or Base
-    //6. Cannot attempt to eat larger animal
+    // rules for general:
+    // 1. Cannot move more than one block
+    // 2. Cannot move on the spot 
+    // (a.k.a choose to pick up the pawn and put it back to original posision)
+    // 3. Cannot move outside of chess board
+    // 4. Cannot enter river
+    // 5. Cannot move into friendly animal or trap or Base
+    // 6. Cannot attempt to eat larger animal
     public void checkIsValidMove(int xdist, int ydist) throws InvalidMovementException{
         if ((Math.abs(x-xdist) + Math.abs(y-ydist)) > 1) {
             throw new InvalidMovementException("Cannot move more than one block");
