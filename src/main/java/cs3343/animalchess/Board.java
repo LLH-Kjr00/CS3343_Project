cd2dc463dfe1dec7430cacc6e318ef0ca353af76
Landon Chow <landonchow@gmail.com>
2023-10-03 21:50:13 +0800

    new_branch

---
+++ src/main/java/cs3343/animalchess/Board.java
@@ -0,0 +1,12 @@
+package cs3343.animalchess;
+
+public class Board {
+    public Board(){};
+
+    public void addAnimal2Board(Animal piece, int x, int y){
+
+    }
+    public Animal getLocationInfo(int x, int y) {
+
+    }
+}
