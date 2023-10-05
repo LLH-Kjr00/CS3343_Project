a9b29f1bb4d9c0d09f77d264e77e593dec26dd71
clayclaw <nope>
2023-10-05 20:24:21 +0800

    enable scheduling

--- src/main/java/cs3343/animalchess/AnimalChessApplication.java
+++ src/main/java/cs3343/animalchess/AnimalChessApplication.java
@@ -2,8 +2,10 @@ package cs3343.animalchess;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
-@SpringBootApplication
+@SpringBootApplication()
+@EnableScheduling
 public class AnimalChessApplication {
 
 	public static void main(String[] args) {
