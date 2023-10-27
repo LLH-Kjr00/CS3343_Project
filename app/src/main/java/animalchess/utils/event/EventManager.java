342c9fd3d5a33e0ec8f5973c2063b8623fabc153
clayclaw <nope>
2023-10-28 00:07:32 +0800

    Custom EventObservable implementation

--- app/src/main/java/animalchess/utils/event/EventManager.java
+++ app/src/main/java/animalchess/utils/event/EventManager.java
@@ -1,35 +1,56 @@
 package animalchess.utils.event;
 
+import animalchess.exceptions.InvalidEventHandlerParameterException;
+import animalchess.utils.common.Pair;
 import animalchess.utils.flow.Disposable;
-import animalchess.utils.flow.Observable;
 
+import java.lang.reflect.InvocationTargetException;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.function.Consumer;
 
 public class EventManager {
 
-    private final HashMap<Class<? extends Event>, Observable<? extends Event>> eventObservables = new HashMap<>();
-    private final HashMap<Class<? extends Event>, EventPriority> eventPriorities = new HashMap<>();
+    private final HashMap<Class<? extends Event>, ObservableEvent<?>> eventObservables = new HashMap<>();
 
     public EventManager() {}
 
-    public <T extends Event> void register(Class<T> eventClass) {
+    public <T extends Event> void registerEvent(Class<T> eventClass) {
         if(eventObservables.containsKey(eventClass)) {
             throw new IllegalArgumentException("Event already registered");
         }
-        eventObservables.put(eventClass, new Observable<T>(null));
+        eventObservables.put(eventClass, new ObservableEvent<T>());
     }
 
     public <T extends Event> void push(T event) {
         if(!eventObservables.containsKey(event.getClass())) {
             throw new IllegalArgumentException("Event not registered");
         }
-        Observable<T> observable = (Observable<T>) eventObservables.get(event.getClass());
+        ObservableEvent<T> observable = (ObservableEvent<T>) eventObservables.get(event.getClass());
         observable.update(event);
     }
 
     public <T extends Event> Disposable listen(Class<T> eventClass, EventPriority priority, Consumer<T> consumer) {
-        return ((Observable<T>) eventObservables.get(eventClass)).subscribe(consumer);
+        return ((ObservableEvent<T>) eventObservables.get(eventClass)).subscribe(new Pair<>(priority, consumer));
+    }
+
+    public void registerListeners(Object listenerInstance) {
+        Arrays.stream(listenerInstance.getClass().getMethods())
+                .filter(method -> method.isAnnotationPresent(EventHandler.class))
+                .forEach((method) -> {
+                    EventHandler eventHandler = method.getAnnotation(EventHandler.class);
+                    if(method.getParameterCount() != 1 || !method.getParameterTypes()[0].isAssignableFrom(Event.class)) {
+                        throw new InvalidEventHandlerParameterException(listenerInstance.getClass(), method);
+                    }
+                    Class<? extends Event> eventClass = (Class<? extends Event>) method.getParameterTypes()[0];
+                    listen(eventClass, eventHandler.priority(), (eventInstance) -> {
+                        try {
+                            method.invoke(listenerInstance, eventInstance);
+                        } catch (IllegalAccessException | InvocationTargetException e) {
+                            e.printStackTrace();
+                        }
+                    });
+                });
     }
 
 }
