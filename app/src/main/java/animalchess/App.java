ddec7137c7c301792af4510a767aa633817b824a
clayclaw <nope>
2023-10-27 20:48:55 +0800

    add service access method in main class

--- app/src/main/java/animalchess/App.java
+++ app/src/main/java/animalchess/App.java
@@ -3,14 +3,19 @@
  */
 package animalchess;
 
+import animalchess.board.Board;
+import animalchess.game.GameUI;
 import animalchess.utils.EventManager;
 
 public class App {
 
     private static EventManager eventManager;
+    private static GameUI ui;
+    private static Board board;
 
     public static void main(String[] args) {
         eventManager = new EventManager();
+        ui = GameUI.getInstance();
     }
 
     public static EventManager getEventManager() {
