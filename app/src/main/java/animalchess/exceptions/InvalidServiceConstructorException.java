6d6ca151a632b42c0c0e484b0aa2829acb698a39
Kjr00 <89450959+Kjr00@users.noreply.github.com>
2023-11-14 07:38:53 +0800

    Deleted some exceptions

--- app/src/main/java/animalchess/exceptions/InvalidServiceConstructorException.java
+++
@@ -1,7 +0,0 @@
-package animalchess.exceptions;
-
-public class InvalidServiceConstructorException extends RuntimeException {
-    public InvalidServiceConstructorException(Class<?> serviceClass) {
-        super("Invalid constructor for service " + serviceClass.getName() + ". Service must have a single constructor.");
-    }
-}
