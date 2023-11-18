dd7d860d8a60436dbfaf4f2bda19568f3e28f928
Kjr00 <89450959+Kjr00@users.noreply.github.com>
2023-11-18 19:10:51 +0800

    minor changes on JumpingAnimal

--- app/src/main/java/animalchess/animals/JumpingAnimal_Actions.java
+++ app/src/main/java/animalchess/animals/JumpingAnimal_Actions.java
@@ -3,5 +3,5 @@ package animalchess.animals;
 import animalchess.exceptions.InvalidMovementException;
 
 public interface JumpingAnimal_Actions {
-	abstract boolean checkIsValidJump (int xdist, int ydist) throws InvalidMovementException;
+	public boolean checkIsValidJump (int xdist, int ydist) throws InvalidMovementException;
 }
